5b1a114435a085c9209d9a012796baab08b09085
Marla Sowards <marla.sowards@healthcatalyst.com>
2018-07-18 12:00:58 -0600

    js own script, add date in grouped row, consolidate escape " scripts

---
+++ main.js
@@ -0,0 +1,164 @@
+$(document).ready(function () {
+    $.getJSON('test.json', function (json) {
+        var tr;
+        for (var i = 0; i < json.length; i++) {
+            tr = $('<tr/>');
+            tr.append("<td>" + json[i].software + "</td>");
+            tr.append("<td>" + json[i].fullName + "</td>");
+            tr.append("<td>" + json[i].softwareAbbrev + "</td>");
+            tr.append("<td>" + json[i].releaseName + "</td>");
+            tr.append("<td>" + json[i].sqlServerVersion + "</td>");
+            tr.append("<td>" + json[i].releaseDate + "</td>");
+            tr.append("<td>" + json[i].type + "</td>");
+            tr.append("<td>" + json[i].releaseNote + "</td>");
+            tr.append("<td>" + json[i].productsAffected + "</td>");
+            tr.append("<td>" +
+                "<a href=\"https://healthcatalyst.visualstudio.com/CAP/_workitems/edit/" +
+                json[i].id + "\" target=\"_blank\">" + json[i].id + "</a>" + "</td>");
+            $('table').append(tr);
+        }
+        var groupColumn = 1;
+        var table = $('#notes').DataTable({
+            //orderFixed: [2, 'asc'],
+            keys: true,
+            //stateSave: true,
+            responsive: true,
+            colReorder: true,
+            select: true,
+            fixedHeader: true,
+            "columnDefs": [{
+                "visible": false,
+                "targets": groupColumn
+            }],
+            buttons: [
+                'copy', 'excel', 'csv'
+            ],
+            //Change order of elements on page
+            dom: 'frBtip',
+            "displayLength": 5000,
+            "order": [
+                [groupColumn, 'desc']
+            ],
+            select: {
+                style: 'os',
+                //Deselect when you click outside the table
+                blurable: true
+            },
+
+            "drawCallback": function (settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+
+                api.column(groupColumn, {
+                    page: 'current'
+                }).data().each(function (group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before(
+                            '<tr class="group"><td colspan="9">' +
+                            group + '</td></tr>'
+                        );
+
+                        last = group;
+                    }
+                });
+            }
+        });
+
+        // Order by the grouping
+        $('#notes tbody').on('click', 'tr.group', function () {
+            var currentOrder = table.order()[0];
+            if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+                table.order([groupColumn, 'desc']).draw();
+            } else {
+                table.order([groupColumn, 'asc']).draw();
+            }
+        });
+
+        //yadcf instructions
+        'use strict';
+        var oTable = $('#notes').DataTable();
+        yadcf.init(oTable, [{
+                    // Software
+                    column_number: 0,
+                    filter_type: 'multi_select',
+                    select_type: 'chosen',
+                    filter_match_mode: 'exact'
+                },
+                {
+                    // Full name (not shown by default because table display is grouped on these rows)
+                    column_number: 1,
+                    filter_type: 'multi_select',
+                    select_type: 'chosen',
+                    filter_match_mode: 'exact'
+                },
+                {
+                    // Version (aka root version number, e.g., DOS 18.1)
+                    column_number: 2,
+                    filter_type: 'multi_select',
+                    select_type: 'chosen',
+                    filter_match_mode: 'exact',
+                    sort_as: "desc"
+                },
+                {
+                    // Release name (aka version number)
+                    column_number: 3,
+                    filter_type: 'multi_select',
+                    select_type: 'chosen',
+                    sort_order: 'asc',
+                    sort_as: 'asc',
+                    filter_match_mode: 'exact'
+                },
+                {
+                    // SQL Server version
+                    column_number: 4,
+                    filter_type: 'multi_select',
+                    select_type: 'chosen',
+                    filter_match_mode: 'exact',
+                    sort_order: 'asc',
+                    dropdownAutoWidth: true
+                },
+                {
+                    // Release date
+                    column_number: 5,
+                    filter_type: 'multi_select',
+                    sort_order: 'asc',
+                    filter_match_mode: 'exact',
+                    select_type: 'chosen'
+                },
+                {
+                    // Type
+                    column_number: 6,
+                    filter_type: 'multi_select',
+                    filter_match_mode: 'exact',
+                    select_type: 'chosen'
+                },
+                //{
+                //    Release note
+                //    column_number: 7,
+                //},            
+                {
+                    // Products affected
+                    column_number: 8,
+                    column_data_type: "html",
+                    html_data_type: "selector",
+                    filter_default_label: "Select product",
+                    data: ['Atlas', 'CAP SQL 2016', 'DMCM', 'EDW Console', 'Fabric', 'IDEA',
+                        'Install', 'Loader Engine', 'Platform General', 'SAMD',
+                        'Services', 'SMD'
+                    ]
+                }
+                //,{
+                //    ID
+                //    column_number: 9,
+                //},
+            ]
+            // Cumulative not recommended because user can't select multiple releases
+            //{
+            //    cumulative_filtering: true
+            //}
+        );
+    });
+});
--- script.sh
+++ script.sh
@@ -2,35 +2,28 @@
 #Script to prepare JSON output from release notes step in VSTS releases to be transformed into a dynamic HTML table
 #jq required (https://stedolan.github.io/jq)
 #Stepped for troubleshooting purposes
+#VersionsAffected is not used
 
 #PUT YOUR JSON FILES IN A DIR AT ROOT CALLED `json`
-
 cd json
 
 #Encode
 dos2unix *.json
 
-#Escape quotes - staggered for troubleshooting
-##CSS styles
-sed -i 's|<span style=\"|<span style=\\\"|g' *.json
-#URLs
-sed -i 's|<a href=\"|<a href=\\\"|g' *.json
-#Closing angle brackets
-sed -i 's|\">|\\\">|g' *.json
+#Escape quotes (including hrefs within values)
+sed -i -E 's/"/\\&/4g; s/\\"(,)?$/"\1/' *.json
 
+#Bold to code style
+##Strange corner case in 18.1.18165.03 for 2012 and 2016 (line is removable if not working in CAP VSTS project)
+sed -i -E 's|int.<b>|int.</b>|g;' *.json
 #Bold to code style
 sed -i -E 's|<span style=\\\"font-weight:bold;\\\">([^<]*)</span>|<code>\1</code>|g;' *.json
 sed -i -E 's|<b>([^<]*)</b>|<code>\1</code>|g;' *.json
 sed -i -E 's|<b>([^<]*)<b/>|<code>\1</code>|g;' *.json
-
 ##To address periods in strings
 sed -i -E 's|<span style=\\\"font-weight: bold;\\\">([a-zA-Z0-9:& ]*)\.([a-zA-Z0-9:& ]*)|<code>\1\2</code>|g;' *.json
 
-##Removable if not working in CAP VSTS project: Strange corner case in 18.1.18165.03 for 2012 and 2016
-sed -i -E 's|<b>Arithmetic|Arithmetic|g;' *.json
-sed -i -E 's|int.<b>|<code>int.</code>|g;' *.json
-
-#Removable if not working in CAP VSTS project: Two early releases were called CAP 18.1 - replace with DOS 18.1
+#Two early releases were called CAP 18.1 - replace with DOS 18.1 (line is removable if not working in CAP VSTS project)
 sed -i 's|CAP 18|DOS 18|g;' *.json
 
 #JQ does not allow inplace - combines *.json into one
@@ -64,8 +57,8 @@ jq '[.[] | .software |= sub(" (.*?) for SQL 20.. triggered on(.*?)......-......"
 jq '[.[] | .software |= sub(" (.*?) triggered on(.*?)......-......";"")]' <10.json >11.json
 
 #New column to abbreviate "Software"
-##Duplicate fullName field
-jq '.[] |= {releaseDate,fullName,sqlServerVersion,software,"softwareAbbrev":.fullName,releaseName,releaseNote,type,productsAffected,id}' <11.json >12.json
+##Duplicate fullName field, combine full name and release date
+jq '.[] |= {releaseDate,fullName: (.fullName + ", " + .releaseDate),sqlServerVersion,software,"softwareAbbrev":.fullName,releaseName,releaseNote,type,productsAffected,id}' <11.json >12.json
 ##Remove SQL Server version for CAP and DOS
 jq '[.[] | .softwareAbbrev |= sub(" for SQL 20.."; "")]' <12.json >13.json
 jq '[.[] | .softwareAbbrev |= match("[A-Z]* [0-9]*[0-9].[0-9]").string]' <13.json >14.json
